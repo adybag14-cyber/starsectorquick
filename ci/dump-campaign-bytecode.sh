c0559a037b8c16b1da3fe273fa5f8f5d416bd89e
adybag14-cyber <adybag14@gmail.com>
2026-08-08 22:18:24 +0100

    Capture CampaignFleet constructor dependencies

--- ci/dump-campaign-bytecode.sh
+++ ci/dump-campaign-bytecode.sh
@@ -8,6 +8,7 @@ classes=(
   data.scripts.world.SectorGen
   com.fs.starfarer.campaign.CustomCampaignEntity
   com.fs.starfarer.campaign.BaseLocation
+  com.fs.starfarer.campaign.BaseCampaignEntity
   com.fs.starfarer.api.impl.campaign.CoreLifecyclePluginImpl
   com.fs.starfarer.api.impl.campaign.ids.People
   com.fs.starfarer.api.util.Misc
@@ -16,9 +17,20 @@ classes=(
   com.fs.starfarer.campaign.save.CampaignGameManager
   com.fs.starfarer.campaign.CampaignEngine
   com.fs.starfarer.campaign.CampaignState
+  com.fs.starfarer.campaign.Faction
+  com.fs.starfarer.campaign.CharacterStats
+  com.fs.starfarer.rpg.Person
   com.fs.starfarer.campaign.fleet.CampaignFleet
+  com.fs.starfarer.campaign.fleet.MutableFleetStats
   com.fs.starfarer.campaign.fleet.FleetMember
   com.fs.starfarer.campaign.fleet.FleetData
+  com.fs.starfarer.campaign.fleet.CampaignFleetView
+  com.fs.starfarer.campaign.fleet.SmoothMovementModule
+  com.fs.starfarer.campaign.fleet.SmoothFacingModule
+  com.fs.starfarer.campaign.accidents.AccidentManager
+  com.fs.starfarer.campaign.fleet.LogisticsModule
+  com.fs.starfarer.campaign.o0OO
+  com.fs.graphics.Sprite
   com.fs.starfarer.campaign.F
   com.fs.starfarer.BaseGameState
   com.fs.state.AppDriver
